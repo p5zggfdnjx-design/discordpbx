68123efb1695586f12996ad7c2653b7348d43abe
p5zggfdnjx-design <p5zggfdnjx@privaterelay.appleid.com>
2026-08-26 07:58:11 -0400

    Fix updater hotfix startup module alias

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 import runpy
+import sys
+
+# runtime_hotfix was originally written against an internal module name that is
+# not present in packaged releases. Alias the real webui module before applying
+# the hotfix so startup cannot fail with ModuleNotFoundError.
+import webui
+sys.modules.setdefault("webui_v3", webui)
 
 from runtime_hotfix import apply
 
